e6f6c35d7082c113479d12a0eef3d63c0bc3e117
TheJis <62614133+ing-thejis@users.noreply.github.com>
2021-05-07 20:01:18 -0500

    commit initial
    commit initial

---
+++ queue.go
@@ -0,0 +1,60 @@
+package queue
+
+type Node struct {
+	Data interface{}
+	Next *Node
+}
+
+func NewNode(v interface{}) *Node {
+	node := new(Node)
+	node.Data = v
+	node.Next = nil
+	return node
+}
+
+type Queue struct {
+	header *Node
+	footer *Node
+}
+
+func NewQueue() *Queue {
+	queue := new(Queue)
+	queue.header = nil
+	queue.footer = nil
+	return queue
+}
+
+func (q *Queue) Push(v interface{}) {
+	node := NewNode(v)
+	if q.header == nil {
+		q.header = node
+	} else {
+		q.footer.Next = node
+	}
+	q.footer = node
+}
+
+func (q *Queue) Pop() interface{} {
+	if q.header == nil {
+		print("Queue Empty")
+		return nil
+	}
+	aux := q.header.Data
+	q.header = q.header.Next
+	return aux
+}
+
+func (q *Queue) Header() interface{} {
+	if q.header == nil {
+		return nil
+	}
+	return q.header.Data
+}
+
+func (q *Queue) IsEmpty() bool {
+	if q.header == nil {
+		print("Queue Empty")
+		return true
+	}
+	return false
+}
---
+++ queue_test.go
@@ -0,0 +1,9 @@
+package queue
+
+import (
+	"testing"
+)
+
+func TestNewQueue(t *testing.T) {
+
+}
